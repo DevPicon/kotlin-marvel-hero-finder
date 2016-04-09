734118b54e2b652dff3654a1f3621505a2df7cd5
Armando Picón <picondev@gmail.com>
2016-04-09 15:44:00 -0500

    Proyecto inicial

---
+++ app/src/main/kotlin/pe/androidperu/kotlinproyectobase/MainActivity.kt
@@ -0,0 +1,12 @@
+package pe.androidperu.kotlinproyectobase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
