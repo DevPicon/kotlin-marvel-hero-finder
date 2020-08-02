a1a9e669a83dff5cb15bcd5f49810a9a4aaa97fe
Armando <picondev@gmail.com>
2020-08-02 16:32:57 -0400

    add viewstates classes

--- app/src/main/kotlin/pe/devpicon/android/marvelheroes/presentation/home/MainViewState.kt
+++ app/src/main/kotlin/pe/devpicon/android/marvelheroes/presentation/home/MainViewState.kt
@@ -1,6 +1,21 @@
 package pe.devpicon.android.marvelheroes.presentation.home
 
+import android.text.SpannableString
+
 data class SearchViewState(
         val id: Long,
         val name: String
+)
+
+data class HeroViewState(
+        val thumbnailUrl: String,
+        val name: SpannableString,
+        val description: SpannableString,
+        val comicViewStateList: MutableList<ComicViewState>
+)
+
+data class ComicViewState(
+        val thumbnailUrl: String,
+        val name: SpannableString,
+        val description: SpannableString
 )
